4eaa7e4fdbcd73d0dbf915fd5899347c895d1b64
yoran <69626931+yoran15@users.noreply.github.com>
2024-06-12 00:06:12 +0200

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,5 @@
 <?php
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'test@test.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
